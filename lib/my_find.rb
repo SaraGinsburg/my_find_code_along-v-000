96fff0b02c66b781fc80204e2c894891cd094316
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-08 17:44:48 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -7,5 +7,6 @@ def my_find(collection)
     arr_result << yield(collection[i])
     i += 1
   end
-  arr_result.include?(true) ? collection[arr_result.index(true)] : nil
+  result = arr_result.include?(true) ? collection[arr_result.index(true)] : nil
+
 end
