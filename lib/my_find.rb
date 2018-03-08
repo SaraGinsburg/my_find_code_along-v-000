564e56644f733009948389e2c42ceeab77bf3037
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-08 17:34:32 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  arr_result = []
+  i = 0
+  while i < collection.length
+    arr_result << yield(collection[i])
+    i += 1
+  end
+  arr_result.include?(true) ? arr_result.index(true) : false
 end
